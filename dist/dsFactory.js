f79fe8a9fa3acb26261dbf1b178cf9b5c75a3c38
Adrian von Gegerfelt <adrian@designbyadrian.com>
2019-12-17 09:00:09 +0100

    Added facility icons

---
+++ dist/dsFactory.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "ds";
+var iconName = "factory";
+var width = 512;
+var height = 512;
+var ligatures = [];
+var unicode = "e001";
+var svgPathData =
+  "M151.9,286l-73.3,29.3l10-248.5h55.2L151.9,286z M258.8,96.8h-55.2l-9.7,240.6l72.7-29L258.8,96.8z M512,300  v212H0V368.2L170.8,300v68.1L341.4,300v68.1L512,300z M276.8,458.4h-87.6V492h87.6V458.4z M276.8,404.8h-87.6v33.6h87.6V404.8z   M384.4,458.4h-87.6V492h87.6V458.4z M384.4,404.8h-87.6v33.6h87.6V404.8z M492,458.4h-87.6l0,33.6H492V458.4z M492,404.8h-87.6  l0,33.6H492V404.8z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [width, height, ligatures, unicode, svgPathData]
+};
+
+exports.dsFactory = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
---
+++ dist/dsWarehouse.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prefix = "ds";
+var iconName = "warehouse";
+var width = 512;
+var height = 512;
+var ligatures = [];
+var unicode = "e001";
+var svgPathData =
+  "M502.2,174L263.4,41c-4.9-2.5-11.1-2.5-16,0L8.6,174c-4.9,2.4-8.6,8.6-8.6,14.8v4.9c0,12.3,13.5,20.9,24.6,14.8  l8.6-4.9v292.5c0,8.6,7.4,16,17.2,16h4.9c9.8,0,17.2-7.4,17.2-17.2V320.5h366.8v174.3c0,9.8,7.4,17.2,17.2,17.2h4.9  c9.8,0,17.2-7.4,17.2-17.2V202.2l8.6,4.9c11.1,6.2,24.6-2.5,24.6-14.8v-4.9C510.8,182.5,508.3,176.3,502.2,174z M72.5,260.5h366.8  v20H72.5V260.5z M72.5,250.5v-20h366.8v20H72.5z M72.5,220.5V200h366.8v20.5H72.5z M72.5,310.5v-20h366.8v20H72.5z M339.3,512h-50  c-5.5,0-10-4.5-10-10v-50c0-5.5,4.5-10,10-10h50c5.5,0,10,4.5,10,10v50C349.3,507.5,344.9,512,339.3,512z M419.3,512h-50  c-5.5,0-10-4.5-10-10v-50c0-5.5,4.5-10,10-10h50c5.5,0,10,4.5,10,10v50C429.3,507.5,424.9,512,419.3,512z M142.5,512h-50  c-5.5,0-10-4.5-10-10v-50c0-5.5,4.5-10,10-10h50c5.5,0,10,4.5,10,10v50C152.5,507.5,148.1,512,142.5,512z M419.3,432h-50  c-5.5,0-10-4.5-10-10v-50c0-5.5,4.5-10,10-10h50c5.5,0,10,4.5,10,10v50C429.3,427.5,424.9,432,419.3,432z";
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [width, height, ligatures, unicode, svgPathData]
+};
+
+exports.dsWarehouse = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
